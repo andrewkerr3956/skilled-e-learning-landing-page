a90fd8ecedbc5eeaccc3bc4973ba2274761361f6
Andrew Kerr <85970993+andrewkerr3956@users.noreply.github.com>
2022-12-15 02:33:30 -0500

    Update Card.tsx

--- src/components/base/Card.tsx
+++ src/components/base/Card.tsx
@@ -12,7 +12,7 @@ const Card = (props: IProps) => {
     const { icon, iconAlt, title, gradientBg, largeTitle, showLink, body } = props;
 
     return (
-        <figure className={`card ${!body ? "popular-card" : null} relative h-[322px]`}>
+        <figure className={`card ${!body ? "popular-card" : null} relative h-[322px] bg-white`}>
             {icon && <img className="left-[32px] -top-[26px] absolute card-icon rounded-full z-10 w-[56px] h-[56px]" src={icon ? icon : "#"} alt={iconAlt ? iconAlt : ""} />}
             <figcaption className={`relative h-full rounded-[10px] md:rounded-[15px] pt-[64px] pb-[40px] px-[32px] bg-${gradientBg ? "orange-gradient" : "white"}`}>
                 {title ? largeTitle ? <h2 className={`card-title ${gradientBg ? "text-white" : null}`}>{title}</h2> : <h3 className="card-title">{title}</h3> : <h3>Untitled</h3>}
