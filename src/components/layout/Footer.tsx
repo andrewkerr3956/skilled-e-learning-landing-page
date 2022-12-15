af556e5ad872eb6ce39fbee26357618daa84a47f
Andrew Kerr <andrewkerr3956@gmail.com>
2022-12-15 01:35:50 -0500

    fix design issues

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = (
     <footer className="w-full bg-navy">
         <div className="container flex flex-1 justify-between items-center gap-2 sm:gap-0 flex-wrap sm:flex-nowrap text-white py-[32px]">
-            <img src="/assets/logo-light.svg" alt="Skilled Logo" className="block" width="111px" height="auto" />
+            <img src="/assets/logo-light.svg" alt="Skilled Logo" className="block" width={111} />
             <button type="button" className="p-btn rounded-btn text-white bg-blue-gradient">Get Started</button>
         </div>
     </footer>
